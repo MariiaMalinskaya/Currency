cecb0a1faaecbcf866314a59d86e0afaa486e67d
Mariia <mariia@Mariias-MacBook-Pro.local>
2021-07-20 21:44:57 +0300

    Create ContactUs model

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+# class Rate(models.Model):
+#     sale = models.DecimalField(max_digits=5, decimal_places=2)
+#     buy = models.DecimalField(max_digits=5, decimal_places=2)  # 32.45
+#     created = models.DateTimeField(auto_now_add=True)
+#     source = models.CharField(max_length=32)  # privatbank,monobank
+#     type = models.CharField(max_length=3)  # USD, EUR
+
+
+class ContactUs(models.Model):
+    firstname = models.CharField(max_length=20)
+    lastname = models.CharField(max_length=20)
+    phone = models.IntegerField()
+    mail = models.EmailField(max_length=100)
+
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from currency.views import hello_world
-from currency.views import generate_password
+# from currency.views import generate_password
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     # currency
     path('hello-world/', hello_world),
-    path('gen-pass/', generate_password)
+    # path('gen-pass/', generate_password)
 ]
