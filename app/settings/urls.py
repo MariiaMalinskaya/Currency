305e5247033da16a5453414884fb1455dd8781dc
Mariia <mariia@Mariias-MacBook-Pro.local>
2021-07-26 18:05:27 +0300

    add flake8 refactor files

--- app/settings/urls.py
+++ app/settings/urls.py
@@ -13,10 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
+from currency.views import hello_world
+
 from django.contrib import admin
 from django.urls import path
 
-from currency.views import hello_world
 # from currency.views import generate_password
 
 urlpatterns = [
